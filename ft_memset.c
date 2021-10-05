8fa9022743f5297f30d064186ef6ec648ab6a2d0
Niyaz <niazlv03@gmail.com>
2021-10-05 20:55:37 +0300

    added ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,33 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahector <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/05 17:03:39 by ahector           #+#    #+#             */
-/*   Updated: 2021/10/05 20:54:39 by ahector          ###   ########.fr       */
+/*   Created: 2021/10/05 20:41:31 by ahector           #+#    #+#             */
+/*   Updated: 2021/10/05 20:55:14 by ahector          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef LIBFT_H
-# define LIBFT_H
 
-# include <string.h>
-# include <unistd.h>
-# include <stdlib.h>
+#include "libft.h"
 
-int			ft_isupper(int c);
-int			ft_islower(int c);
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *str;
 
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isascii(int c);
-int			ft_isprint(int c);
-
-void		*ft_memset(void *b, int c, size_t len);
-
-size_t		ft_strlen(const char *s);
-
-
-#endif
+	str = (unsigned char *)b;
+	while(len)
+	{
+		*str = (unsigned char)c;
+		//НЕДОПИСАНО!!!!
+	}
+}
