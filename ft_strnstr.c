ee08e53ca5ae49845f605dcfb87d6eb7e78d5198
Niyaz <niazlv03@gmail.com>
2021-10-09 19:13:08 +0300

    try write ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahector <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 19:07:42 by ahector           #+#    #+#             */
+/*   Updated: 2021/10/09 19:12:32 by ahector          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t  i;
+	size_t  j;
+
+	i = 0;
+	j = 0;
+	if (!*needle)
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		if (haystack[i] == needle[0])
+		{
+			while (needle[j] != '\0' && haystack[i + j] == needle[j] && i + j < len)
+			{
+				if (needle[j + 1] == '\0')
+					return((char *)&haystack[i]);
+				j++;
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
