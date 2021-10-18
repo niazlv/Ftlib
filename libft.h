47e30e54766bad24de3c8c9972bc87e321f52653
Niyaz <niazlv03@gmail.com>
2021-10-18 19:56:32 +0300

    added ft_strtrim

--- libft.h
+++ libft.h
@@ -53,5 +53,6 @@ void		ft_putnbr_fd(int n, int fd);
 
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
 
 #endif
