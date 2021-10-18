75b3e1c3218f24da7f13a616c4b93df4a00205bf
Niyaz <niazlv03@gmail.com>
2021-10-18 18:14:06 +0300

    fixed ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ahector <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 17:17:39 by ahector           #+#    #+#             */
-/*   Updated: 2021/10/11 17:28:10 by ahector          ###   ########.fr       */
+/*   Updated: 2021/10/18 18:13:30 by ahector          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	c;
+	unsigned int	num;
 
-	if (n > 0)
+	if (n < 0)
 	{
-		while(n/10 > 0)
-			
+		ft_putchar_fd('-', fd);
+		num =(unsigned)-n;
 	}
-	else if (n < 0)
-	{
-		while()
-	}	
 	else
+		num = n;
+	if (num >= 10)
 	{
-		write(fd, "0", 1);
+		ft_putnbr_fd(num/10, fd);
+		ft_putchar_fd((num % 10) + 48, fd);
 	}
+	else
+		ft_putchar_fd(num + 48, fd);
 }
