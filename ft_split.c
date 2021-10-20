4954dbe4147c5e441c5776e0f02e67660eeb2a13
Niyaz Huawei Matebook(kali-linux) <niazlv03@gmail.com>
2021-10-20 11:26:28 +0300

    added ft_split(modifyed) and updated Makefile

--- ft_split.c
+++ ft_split.c
@@ -1,25 +1,47 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ahector <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/18 20:13:16 by ahector           #+#    #+#             */
-/*   Updated: 2021/10/18 20:24:44 by ahector          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 char	**ft_split(char const *s, char c)
 {
-	char	*str;
-	char	**strstr;
-	size_t	i;
+	char	**str;
+	int	i;
+	int	count;
+	int	j;
+	int	u;
 
 	i = 0;
-	while(s[i] != c)
+	count = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if ((char)s[i] == c)
+			count++;
 		i++;
-	str = (char *)malloc(sizeof(char) * (i + 1));
+	}
+	str = (char **)malloc(sizeof(char *) * (count + 1 + 1));
+	i = 0;
+	j = 0;
+	u = 0;
+	while (i < count + 1)
+	{
+		while(s[j] != c)
+			j++;
+		str[i++] = (char *)malloc(sizeof(char *) * j++ + 1);
+	}
+	i = -1;
+	j = 0;
+	while (i++ < count)
+	{
+		u = 0;
+		while(s[j] != c)
+		{
+			str[i][u] = *(s+count+(1 * i) + j);
+
+			u++;
+			j++;
+		}
+		str[i][u] = 0;
+	}
+	str[i] = NULL;
+	return (str);
 }
